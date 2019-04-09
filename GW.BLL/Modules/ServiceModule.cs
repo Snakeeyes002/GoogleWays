2f44ed009ffc04014bb5a7d68dcdd55fa344920d
Student <student@lan.itstep.kiev.ua>
2019-04-09 11:37:02 +0300

    Add Module in GW.BLL

---
+++ GW.BLL/Modules/ServiceModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using GW.BLL.Models;
+using GW.BLL.Services;
+using GW.DAL.DbLayer;
+using GW.DAL.Repositories;
+using GW.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Module = Autofac.Module;
+
+namespace GW.BLL.Modules
+{
+    public class ServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Address
+            builder.RegisterType(typeof(AddressService))
+                            .As(typeof(IGenericService<AddressDTO>));
+            builder.RegisterType(typeof(AddressRepository))
+                          .As(typeof(IGenericRepository<Address>));
+            //Street
+            builder.RegisterType(typeof(StreetService))
+                .As(typeof(IGenericService<StreetDTO>));
+            builder.RegisterType(typeof(StreetRepository))
+                          .As(typeof(IGenericRepository<Street>));
+            //Subdivision
+            builder.RegisterType(typeof(SubdivisionService))
+                .As(typeof(IGenericService<SubdivisionDTO>));
+            builder.RegisterType(typeof(SubdivisionRepository))
+                          .As(typeof(IGenericRepository<Subdivision>));
+
+
+            builder.RegisterType(typeof(GWContext))
+                         .As(typeof(DbContext)).InstancePerLifetimeScope();
+        }
+    }
+}
