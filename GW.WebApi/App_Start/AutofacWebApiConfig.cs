aae63f61a2e87a521499cc2b17559fef4659404c
Student <student@lan.itstep.kiev.ua>
2019-04-09 12:01:56 +0300

    Add class AutofacWebApiConfig

---
+++ GW.WebApi/App_Start/AutofacWebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using GW.BLL.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace GW.WebApi.App_Start
+{
+    public class AutofacWebApiConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            //!!!!!!!!!!!!!!!!регистрируем ВСЕ контроллеры
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            //!!!!!!!!!!!!!!!!регистрируем наш  модуль
+            builder.RegisterModule(new ServiceModule());
+            Container = builder.Build();
+
+            return Container;
+        }
+    }
+}
---
+++ GW.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GW.WebApi.WebApiApplication" Language="C#" %>
