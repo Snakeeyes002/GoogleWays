fbe485f76c5406954eec98e3882e374b349e17a8
Student <student@lan.itstep.kiev.ua>
2019-04-11 11:30:33 +0300

    Autofac added configuration MVC

--- GW.WebUI/Global.asax.cs
+++ GW.WebUI/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using GW.BLL.Modules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +19,15 @@ namespace GW.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Autofac Configuration
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
+            builder.RegisterModule(new ServiceModule());
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
