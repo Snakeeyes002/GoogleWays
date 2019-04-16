f5d965dba575c276172a6b38e93a08d68fffe0a0
Doure <gidroponikaim@gmail.com>
2019-04-16 09:33:26 +0300

    added new models repository

---
+++ GW.DAL/Repositories/UserInRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using GW.DAL.DbLayer;
+using GW.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW.DAL.Repositories
+{
+    public class UserInRoleRepository : GenericRepository<UserInRole>
+    {
+        public UserInRoleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ GW.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using GW.DAL.DbLayer;
+using GW.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW.DAL.Repositories
+{
+    public class UserRepository : GenericRepository<User>
+    {
+        public UserRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
