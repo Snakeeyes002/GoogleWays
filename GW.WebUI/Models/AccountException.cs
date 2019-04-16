aab5d30a7a2e3a1a1b91648ddc380dc8172d07db
Anatoliy <Snakeeyes002@gmail.com>
2019-04-17 01:29:48 +0300

    Identity pattern transfered from authTEST, removed OWIN, removed last version of asp.identity

---
+++ GW.WebUI/Models/AccountException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GW.WebUI.Models
+{
+    public class AccountException : Exception
+    {
+        public AccountException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ GW.WebUI/Models/Models.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GW.WebUI.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Ваша почта")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Ваш пароль")]
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Display(Name = "Ваша почта")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Ваш пароль")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Повторите пароль")]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
