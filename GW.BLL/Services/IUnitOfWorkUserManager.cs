be775e04d746e7b5472b2968ba23ec7e85cf52f9
Doure <gidroponikaim@gmail.com>
2019-04-16 09:59:05 +0300

    added pattern UnitOfWork and UnitOfWorkUserManager

---
+++ GW.BLL/Services/IUnitOfWorkUserManager.cs
@@ -0,0 +1,18 @@
+﻿using GW.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace GW.BLL.Services
+{
+    public interface IUnitOfWorkUserManager : IUnitOfWork
+    {
+        TransactionScope Transaction { get; set; }
+        IGenericService<UserDTO> UserService { get; set; }
+        IGenericService<RoleDTO> RoleService { get; set; }
+        IGenericService<UserInRoleDTO> UserInRoleService { get; set; }
+    }
+}
